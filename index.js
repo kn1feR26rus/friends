3c5196bab08e7799d6e30cfdeebf8f4c27c4db11
Oleg <kn1feR26rus@yandex.ru>
2022-07-12 16:56:04 +0300

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  new Swiper(".swiper", {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    slidesPerColumnFill: "row",
+    spaceBetween: 16,
+    observer: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $("#header_nav-btn").click(function () {
+    $(this).toggleClass("open");
+    $(".full_menu").toggleClass("open_menu");
+    $("body").toggleClass("overflow");
+
+    if (!$(this).hasClass("open")) {
+      $(".full_menu").removeClass("open_menu");
+    }
+  });
+
+  $(".item_url").click(function () {
+    $("#header_nav-btn").toggleClass("open");
+    $(".full_menu").toggleClass("open_menu");
+    $("body").toggleClass("overflow");
+    if (!$(this).hasClass("open")) {
+      $(".full_menu").removeClass("open_menu");
+    }
+  });
+});
